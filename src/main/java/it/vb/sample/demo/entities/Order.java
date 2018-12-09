4d73d69526348386faf90021b7ca22efc3ec61bb
Vittorio Ballestra <vittorio.ballestra@gmail.com>
2018-12-09 19:32:01 +0100

    some tests

--- src/main/java/it/vb/sample/demo/entities/Order.java
+++ src/main/java/it/vb/sample/demo/entities/Order.java
@@ -3,7 +3,6 @@ package it.vb.sample.demo.entities;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
---
+++ src/test/java/it/vb/sample/demo/OrderControllerTests.java
@@ -0,0 +1,81 @@
+package it.vb.sample.demo;
+
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.hamcrest.Matchers;
+import org.hamcrest.collection.IsCollectionWithSize;
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.vb.sample.demo.controllers.OrderController;
+import it.vb.sample.demo.dto.FindOrderCriteriaDTO;
+import it.vb.sample.demo.dto.OrderDTO;
+import it.vb.sample.demo.dto.OrderLineDTO;
+import it.vb.sample.demo.repositories.OrderRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderControllerTests {
+
+    @Autowired
+    private OrderController orderController;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    public void canCreateAnOrder() {
+        long id = createAnOrder();
+        assertThat(id, Matchers.greaterThan(0L));
+
+        // Leave it cleaned Remove the order
+        orderRepository.deleteById(id);
+    }
+
+	private long createAnOrder() {
+		OrderDTO order = new OrderDTO();
+        Calendar cal = Calendar.getInstance();
+        cal.set(118,5,1,12,0);
+        order.setDate(cal.getTime());
+        order.setBuyerEmail("a@b.it");
+        order.setLines(new ArrayList<>());
+        OrderLineDTO line1 = new OrderLineDTO();
+        line1.setSku("AAA00001");
+        line1.setQty(10.0);
+        long id = orderController.createOrder(order);
+        return id;
+    }
+
+    @Test
+    @Transactional
+    public void canFindOrders() {
+        Calendar cal = Calendar.getInstance();
+        
+
+        long id = createAnOrder();
+        FindOrderCriteriaDTO criteriaDTO = new FindOrderCriteriaDTO();
+        cal.set(118, 4, 1, 12, 0);
+        criteriaDTO.setDateFrom(cal.getTime());
+        cal.set(118, 6, 1, 12, 0);
+        criteriaDTO.setDateTo(cal.getTime());
+
+        List<OrderDTO> orders = orderController.findOrdersInRange(criteriaDTO);
+
+        assertThat(orders,IsCollectionWithSize.hasSize(1));
+        assertThat(orders.get(0).getId(),IsEqual.equalTo(id));
+
+        orderRepository.deleteById(id);
+
+
+    }
+
+}
